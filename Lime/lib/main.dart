10c0f73a0a023df8b24a61169d4383a5c16c7178
rex662624 <zfk.662624@gmail.com>
2018-04-18 13:29:24 +0800

    Update step6
    Display all message on the screen

--- Lime/lib/main.dart
+++ Lime/lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 void main() {
   runApp(new FriendlychatApp());
 }
 
-class FriendlychatApp extends StatelessWidget {
+class FriendlychatApp extends StatelessWidget {//APP的主要進入點
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Friendlychat",
+      theme: new ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+      ),
       home: new ChatScreen(),
     );
   }
@@ -25,7 +28,9 @@ class ChatScreen extends StatefulWidget {                     //modified
 
 class ChatScreenState extends State<ChatScreen> {
   //new
-//主體 build
+
+
+  //主體 build
 
   @override //new
   Widget build(BuildContext context) {
@@ -33,16 +38,45 @@ class ChatScreenState extends State<ChatScreen> {
       appBar: new AppBar(
           title: new Text("Lime")
       ),
-      body: _buildTextComposer(), //文字輸入處
+
+      body: new Column(
+        children: <Widget>[
+          new Flexible(//顯示大家的message
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(7.0),//邊邊要留的空白
+              reverse: true,//因為list要從新的到舊的印出來
+              itemBuilder: (_, int index) => globalmessages[index],//_表示unused，
+              itemCount: globalmessages.length,//訊息長度
+            ),
+          ),
+          new Divider(height: 5.0),//分隔線
+          new Container(//自己目前要打的message
+            decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
     );
   }
 
+
+
 //文字輸入框
+  final List<ChatMessage> globalmessages = <ChatMessage>[];//globalmessage代表要顯示的所有訊息
   final TextEditingController _textController = new TextEditingController();
 
   void _handleSubmitted(String text) {
     //submit message要做的事情
-    _textController.clear();
+    _textController.clear();//清掉textfield的
+
+    //並加入messagelist以顯示在螢幕上
+    ChatMessage mymessage = new ChatMessage(
+      text: text,
+    );
+    setState(() {//在這裡動態顯示message(用setstate告訴famework知道這個部分被改變)
+      globalmessages.insert(0,mymessage);
+    });
   }
 
   Widget _buildTextComposer() {
@@ -70,7 +104,41 @@ class ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
-      ), //new
+      ),
+    );
+  }
+}
+
+//----------------------message display
+const String _name = "rex662624";//名字
+
+
+class ChatMessage extends StatelessWidget {//聊天時顯示的訊息格式
+  ChatMessage({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,//自動對齊用的
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),//圓形的大頭貼(會顯示name的第一個字元 所以用name[0])
+          ),
+          new Column(//包起來的部分合成一個container
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
